b14e6f91d876599434b5ebd66c743608ef640b2b
Huseyn Gadirov <huseyngadirov@gmail.com>
2016-07-19 16:55:19 -0800

    Add a service worker

--- app/app.es6
+++ app/app.es6
@@ -1,2 +1,14 @@
 const app = angular.module('ReMimb', ['ngRoute', 'firebase', 'ngAnimate', 'ngFileUpload', 'angularSpinner'])
 
+
+/* Service Worker */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
--- app/app.js
+++ app/app.js
@@ -2,3 +2,14 @@
 'use strict';
 
 var app = angular.module('ReMimb', ['ngRoute', 'firebase', 'ngAnimate', 'ngFileUpload', 'angularSpinner']);
+
+/* Service Worker */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  })['catch'](function (err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
